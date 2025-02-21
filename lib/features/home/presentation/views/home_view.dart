409a83fbe77bb9c82dff14de6728113ab0091305
reemahmad <reemmahmad40@gmail.com>
2025-02-21 18:54:11 +0200

    edit

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -8,7 +8,6 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: HomeViewBody(),
-      //bottomNavigationBar: CustomBottomNavigationBar(),
 
       );
   }
